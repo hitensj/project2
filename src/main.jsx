d302b33806dfa4dfaa42e4ddc000d30ed3e4dc39
Hiten Singh Jethwani <hitensinghjethwani@gmail.com>
2025-07-18 12:39:16 +0530

    cjs file upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // <--- CRUCIAL: This imports your calculator component
+import './index.css'; // <--- CRUCIAL: This imports your Tailwind and custom font styles
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App /> {/* <--- CRUCIAL: This renders your App component */}
+  </React.StrictMode>,
+);
